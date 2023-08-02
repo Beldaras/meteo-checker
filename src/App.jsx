83434e3b928a077dae8b4b221d5da51b50c0fad4
Beldaras <jphilippe.roblot@gmail.com>
2023-08-02 15:31:49 +0200

    Set city name and city coord

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,21 @@ import { useState } from 'react'
 import axios from 'axios'
 
 function App() {
-  const [city, setCity] = useState('Paris');
+  const [cityCoord, setCityCoord] = useState('');
+  const [cityName, setCityName] = useState('');
   useEffect(() => {
     axios.get(`http://api.openweathermap.org/geo/1.0/direct?q=Paris&appid=${import.meta.env.VITE_METEO_KEY}`)
-         .then((response) => response.data);
+         .then((response) => response.data)
+         .then((data) =>  { 
+              setCityCoord("lon : " + data[0].lon.toString() + " / lat : " + data[0].lat.toString());
+              setCityName(data[0].name)
+            });    
   }, []);
   return (
     <>
       <h1>Meteo Checker</h1>
-      <p>City: {city}</p>
-
+      <p>Ville: {cityName}</p>
+      <p>Coordonnées = {cityCoord}</p>
     </>
   )
 }
