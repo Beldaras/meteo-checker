54891b2c7d536d4d5cc360867b5a513cdff28010
Beldaras <jphilippe.roblot@gmail.com>
2023-08-02 15:38:59 +0200

    add input an handlechange to enter a city

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,26 @@ import { useState } from 'react'
 import axios from 'axios'
 
 function App() {
+  const [city, setCity] = useState('');
   const [cityCoord, setCityCoord] = useState('');
   const [cityName, setCityName] = useState('');
+
+  const handleCityChange = (event) => {
+    setCity(event.target.value);
+  }
+
   useEffect(() => {
-    axios.get(`http://api.openweathermap.org/geo/1.0/direct?q=Paris&appid=${import.meta.env.VITE_METEO_KEY}`)
+    axios.get(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=${import.meta.env.VITE_METEO_KEY}`)
          .then((response) => response.data)
          .then((data) =>  { 
               setCityCoord("lon : " + data[0].lon.toString() + " / lat : " + data[0].lat.toString());
               setCityName(data[0].name)
             });    
-  }, []);
+  }, [city]);
   return (
     <>
       <h1>Meteo Checker</h1>
+      <input type="text" value={city} onChange={handleCityChange} />
       <p>Ville: {cityName}</p>
       <p>Coordonnées = {cityCoord}</p>
     </>
