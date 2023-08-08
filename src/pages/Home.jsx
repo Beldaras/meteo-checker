2641e8cb2bd384bc11acc6f722b59107035433fc
Beldaras <jphilippe.roblot@gmail.com>
2023-08-08 10:20:55 +0200

    Modif CSS and add CityCard component to Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import { useEffect } from "react";
 import { useState } from "react";
 import axios from "axios";
 import styles from "../styles/Home.module.css";
+import CityCard from "../components/CityCard";
 
 function Home() {
   const [city, setCity] = useState("");
@@ -81,26 +82,14 @@ function Home() {
         </form>
 
         {cityCoord && (
-          <div>
-            <p>Ville: {cityCoord.name}</p>
-            <p>
-              <span className={styles.coord}>Lat : {cityCoord.lat}</span>
-              <span>Lon : {cityCoord.lon}</span>
-            </p>
-            <img
-              src={`https://openweathermap.org/img/wn/${weather.icon}@4x.png`}
-              alt="image de la météo actuelle"
-            />
-            <p>{weather.description}</p>
-            <p>
-              Température : {temp.temp}°C Ressentie : {temp.feels_like}°C
-            </p>
-            <p>
-              Min : {temp.temp_min}°C Max : {temp.temp_max}°C
-            </p>
-            <p>Levé du soleil : {localizedSunRise}</p>
-            <p>Couché du soleil : {localizedSunSet}</p>
-          </div>
+          <CityCard 
+            cityCoord={cityCoord}
+            weather={weather}
+            temp={temp}
+            localizedSunRise={localizedSunRise}
+            localizedSunSet={localizedSunSet}
+
+          />
         )}
       </div>
     </>
