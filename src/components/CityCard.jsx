3eff47e89d258588ad360ab9b0d293204de86dbd
Beldaras <jphilippe.roblot@gmail.com>
2023-08-31 19:16:51 +0200

    Local storage done

--- src/components/CityCard.jsx
+++ src/components/CityCard.jsx
@@ -1,53 +1,7 @@
-import { useState, useEffect } from "react";
-import axios from "axios";
+
 import styles from "../styles/CityCard.module.css";
 
-function CityCard( {city} ) {
-
-  const [lastCity, setLastCity] = useState("");
-  const [cityCoord, setCityCoord] = useState("");
-  const [weather, setWeather] = useState("");
-  const [temp, setTemp] = useState("");
-  const [sunRise, setSunRise] = useState("");
-  const [sunSet, setSunSet] = useState("");
-
-  useEffect(() => {
-    axios
-      .get(
-        `http://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=${
-          import.meta.env.VITE_METEO_KEY
-        }`
-      )
-      .then((response) => {
-        setCityCoord(response.data[0]);
-
-        const { lat, lon } = response.data[0];
-
-        axios
-          .get(
-            `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${
-              import.meta.env.VITE_METEO_KEY
-            }&units=metric&lang=fr`
-          )
-          .then((data) => {
-            setWeather(data.data.weather[0]);
-            setTemp(data.data.main);
-            setSunRise(data.data.sys.sunrise);
-            setSunSet(data.data.sys.sunset);
-          });
-      });
-  }, [city]);
-
-    useEffect(() => {
-      const lastCity = JSON.parse(localStorage.getItem('lastCity'));
-      if (lastCity) {
-       setLastCity(lastCity);
-      }
-    }, [lastCity]);
-
-    useEffect(() => {
-      localStorage.setItem('lastCity', JSON.stringify(city));
-    }, [city]);
+function CityCard( {cityCoord, weather, temp, sunRise, sunSet} ) {
 
   const localizedSunRise = new Date(sunRise * 1000).toLocaleTimeString(
     "fr-FR",
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,46 @@
 
-import { useState } from "react";
-
+import { useState, useEffect } from "react";
+import axios from "axios";
 import styles from "../styles/Home.module.css";
 import CityCard from "../components/CityCard";
 
 function Home() {
-  const [city, setCity] = useState("");
+  const [city, setCity] = useState(localStorage.getItem('city') || "");
+  // const [lastCity, setLastCity] = useState(city);
+  const [cityCoord, setCityCoord] = useState("");
+  const [weather, setWeather] = useState("");
+  const [temp, setTemp] = useState("");
+  const [sunRise, setSunRise] = useState("");
+  const [sunSet, setSunSet] = useState("");
+
+  useEffect(() => {
+    axios
+      .get(
+        `http://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=${
+          import.meta.env.VITE_METEO_KEY
+        }`
+      )
+      .then((response) => {
+        localStorage.setItem('city', response.data[0].name);
+        setCityCoord(response.data[0]);
+
+        const { lat, lon } = response.data[0];
+
+        axios
+          .get(
+            `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${
+              import.meta.env.VITE_METEO_KEY
+            }&units=metric&lang=fr`
+          )
+          .then((data) => {
+            setWeather(data.data.weather[0]);
+            setTemp(data.data.main);
+            setSunRise(data.data.sys.sunrise);
+            setSunSet(data.data.sys.sunset);
+          });
+      });
+  }, [city]);
+
   
 
   const handleSearch = (event) => {
@@ -21,17 +56,21 @@ function Home() {
     }
   };
 
-  
+ 
   return (
     <>
       <div className={styles.welcome}>
         <h1>Meteo Checker</h1>
         <form onSubmit={handleSearch}>
           <label htmlFor="city">Entrez une ville : </label>
-          <input type="text" id="city" name="city" />
+          <input type="text" id="city" name="city"  />
         </form>
 
-       <CityCard city={city} />
+       <CityCard cityCoord={cityCoord} 
+                 weather={weather}
+                 temp={temp}
+                 sunRise={sunRise}
+                 sunSet={sunSet} />
       </div>
     </>
   );
