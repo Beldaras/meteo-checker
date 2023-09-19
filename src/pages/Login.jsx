ec03d59622f0f207e0ebc56c346fb92758da40a3
Beldaras <jphilippe.roblot@gmail.com>
2023-09-19 10:46:06 +0200

    create pages Login and sign up

---
+++ src/pages/Login.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+
+function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  return (
+    <form>
+      <div>
+        <label htmlFor="email">Enter you email</label>
+        <input
+          type="email"
+          name="email"
+          id="email"
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </div>
+      <div>
+        <label htmlFor="password">Enter you password</label>
+        <input
+          type="password"
+          name="password"
+          id="password"
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </div>
+      <button type="submit">Login</button>
+    </form>
+  );
+}
+
+export default Login;
---
+++ src/pages/SignUp.jsx
@@ -0,0 +1,52 @@
+import { useState } from "react";
+
+function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [firstname, setFirstname] = useState("John");
+  const [lastname, setLastname] = useState("Doe");
+
+  return (
+    <form>
+      <div>
+        <label htmlFor="email">Enter you email</label>
+        <input
+          type="email"
+          name="email"
+          id="email"
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </div>
+      <div>
+        <label htmlFor="password">Enter you password</label>
+        <input
+          type="password"
+          name="password"
+          id="password"
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </div>
+      <div>
+        <label htmlFor="firstname">Enter you firstname</label>
+        <input
+          type="firstname"
+          name="firstname"
+          id="firstname"
+          onChange={(e) => setFirstname(e.target.value)}
+        />
+      </div>
+      <div>
+        <label htmlFor="lastname">Enter you lastname</label>
+        <input
+          type="lastname"
+          name="lastname"
+          id="lastname"
+          onChange={(e) => setLastname(e.target.value)}
+        />
+      </div>
+      <button type="submit">Register</button>
+    </form>
+  );
+}
+
+export default Login;
