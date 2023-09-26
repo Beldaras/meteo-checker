d3797e6e84a74082f9954876665dd5c40743586b
Beldaras <jphilippe.roblot@gmail.com>
2023-09-26 10:45:02 +0200

    little refactor

--- src/components/FavIcon.jsx
+++ src/components/FavIcon.jsx
@@ -3,20 +3,15 @@ import styles from "../styles/FavIcon.module.css";
 import { useAuthContext } from "../contexts/authContext";
 import meteoAPI from "../services/meteoAPI.js";
 
-
 function FavIcon(cityCoord) {
-
   const { user } = useAuthContext();
-  
+
   const handleClick = () => {
-   
-    const city = cityCoord.cityCoord.name;
-    const lat = cityCoord.cityCoord.lat;
-    const lon = cityCoord.cityCoord.lon;
+    const { name, lat, lon } = cityCoord.cityCoord;
+    const city = name;
     const user_id = user.id;
-    console.log(user_id);
-  
-    if(city, lat, lon, user_id){
+
+    if ((city, lat, lon, user_id)) {
       meteoAPI
         .post("/api/fav", { city, lat, lon, user_id })
         .then((res) => {
@@ -25,9 +20,8 @@ function FavIcon(cityCoord) {
         .catch((err) => {
           console.log(err);
         });
-  }
-  }
-
+    }
+  };
 
   return (
     <button className={styles.button} onClick={handleClick}>
