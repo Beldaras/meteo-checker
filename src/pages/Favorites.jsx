cb49d9d13fa89d64fe42f6a9a1f9c212c1b0b66d
Beldaras <jphilippe.roblot@gmail.com>
2023-09-27 13:40:43 +0200

    display user's favorites on fav page

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -1,11 +1,27 @@
 import { useState, useEffect } from "react";
 import { useAuthContext } from "../contexts/authContext";
 import meteoAPI from "../services/meteoAPI.js";
+import axios from "axios";
+import styles from "../styles/Favorites.module.css";
+
 
 function Favorites() {
   const { user } = useAuthContext();
 
   const [favorites, setFavorites] = useState([]);
+  const [weather, setWeather] = useState("");
+  const [temp, setTemp] = useState("");
+  const [sunRise, setSunRise] = useState("");
+  const [sunSet, setSunSet] = useState("");
+
+  const localizedSunRise = new Date(sunRise * 1000).toLocaleTimeString(
+    "fr-FR",
+    { hour: "2-digit", minute: "2-digit" }
+  );
+  const localizedSunSet = new Date(sunSet * 1000).toLocaleTimeString("fr-FR", {
+    hour: "2-digit",
+    minute: "2-digit",
+  });
 
   useEffect(() => {
     meteoAPI
@@ -18,17 +34,56 @@ function Favorites() {
       });
   }, [user.id]);
 
+  useEffect(() => {
+    favorites.map((fav) => {
+      const { lat, lon } = fav;
+      axios
+        .get(
+          `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${
+            import.meta.env.VITE_METEO_KEY
+          }&units=metric&lang=fr`
+        )
+        .then((data) => {
+          console.log(data);
+          setWeather(data.data.weather[0]);
+          setTemp(data.data.main);
+          setSunRise(data.data.sys.sunrise);
+          setSunSet(data.data.sys.sunset);
+        });
+    });
+  }, [favorites]);
+
   console.log(favorites);
 
   return (
-    <div>
+    <div className={styles.welcome}>
       {favorites &&
         favorites.map((fav) => {
           return (
-            <div key={fav.favid}>
-              <h2>{fav.city}</h2>
-              <p>lat : {fav.lat}</p>
-              <p>lon : {fav.lon}</p>
+            <div key={fav.favid} className={styles.card}>
+              <div className={styles.cardHeader}>
+                <p>Ville: {fav.city}</p>
+              </div>
+              <p>
+                <span className={styles.coord}>Lat : {fav.lat}</span>
+                <span className={styles.coord}>Lon : {fav.lon}</span>
+              </p>
+              <div className={styles.description}>
+                <img
+                  className={styles.weather}
+                  src={`https://openweathermap.org/img/wn/${weather.icon}@4x.png`}
+                  alt="image de la météo actuelle"
+                />
+                <p>{weather.description}</p>
+              </div>
+              <p>Température : {temp.temp}°C</p>
+              <p>Ressentie : {temp.feels_like}°C</p>
+              <p>
+                <span className={styles.coord}>Min : {temp.temp_min}°C</span>{" "}
+                <span className={styles.coord}>Max : {temp.temp_max}°C</span>
+              </p>
+              <p>Levé du soleil : {localizedSunRise}</p>
+              <p>Couché du soleil : {localizedSunSet}</p>
             </div>
           );
         })}
